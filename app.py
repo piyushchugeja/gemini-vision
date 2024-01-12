cfbbd9588387f82e71e926578f81c99b429e6d3f
Piyush Chugeja <piyushchugeja@gmail.com>
2024-01-13 01:21:56 +0530

    basic application code upload

---
+++ app.py
@@ -0,0 +1,51 @@
+import google.generativeai as genai
+import streamlit as st
+import PIL.Image
+
+# configure GenAI and initialise model
+genai.configure(
+    api_key=st.secrets["GOOGLE_API_KEY"],
+)
+model = genai.GenerativeModel("gemini-pro-vision")
+
+st.set_page_config(
+    page_title = "Vision",
+    page_icon = "👁️",
+    layout = "wide",
+    initial_sidebar_state = "collapsed",
+)
+
+st.header("Gemini Vision 👁️")
+st.text("Provide an image and a prompt which instructs the AI on what to do with the image. The AI will then generate an output based on the prompt.")
+
+if 'image' not in st.session_state:
+    st.session_state.image = None
+
+col1, col2 = st.columns(2)
+
+with col1:
+    img_raw = st.file_uploader("Upload your image here", type=["png", "jpg", "jpeg"])
+    
+with col2:
+    prompt = st.text_area("Enter your prompt here", placeholder="e.g. Identify the main object in the image and write a blog on it")
+
+button = st.button("Launch")
+
+if button:
+    with st.spinner("Processing..."):
+        try:
+            if img_raw and prompt:
+                st.session_state.image = PIL.Image.open(img_raw)
+                response = model.generate_content(
+                    contents = [prompt, st.session_state.image]
+                )
+                st.success(response.text)
+                st.image(st.session_state.image, use_column_width=True)
+            elif not img_raw:
+                st.error("Please upload an image.")
+                st.session_state.image = None
+            elif not prompt:
+                st.error("Please enter a prompt.")
+        except Exception as e:
+            print(e)
+            st.error("Something went wrong. Please try again.")
